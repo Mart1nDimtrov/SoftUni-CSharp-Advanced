d0481a515e68a203c2f8a0849b2f15f240195856
giggly <giggly@abv.bg>
2018-02-08 23:23:35 +0200

    Exercise - Multidimensional Arrays

---
+++ Exercise - Multidimensional Arrays/2. Diagonal Difference/2. Diagonal Difference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parameters = int.Parse(Console.ReadLine());
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+
+            for (int row = 0; row < parameters; row++)
+            {
+                int[] line = Console.ReadLine()
+                          .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+                for (int col = 0; col < parameters; col++)
+                {
+                    if (row == col)
+                    {
+                        primaryDiagonal += line[col];
+                    }
+                    if (col == (parameters - row) - 1)
+                    {
+                        secondaryDiagonal += line[col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+        }
+    }
+}
---
+++ Exercise - Multidimensional Arrays/Exercise - Multidimensional Arrays/1. Matrix of Palindromes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___Multidimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int rows = parameters[0];
+            int cols = parameters[1];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char ch = Convert.ToChar(row + 97);
+
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write("{0}{1}{2} ", ch, Convert.ToChar(ch + col), ch);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
