24260bd98fcf203a20d430c281bdb72dd78b60e3
giggly <giggly@abv.bg>
2018-02-12 23:50:46 +0200

    Exercise - Dictionaries and LINQ

---
+++ Exercise - Dictionaries and LINQ/Exercise - Dictionaries and LINQ/1. Anonymous Cache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___Dictionaries_and_LINQ
+{
+    class Program
+    {
+        public class DataSet
+        {
+            public string name { get; set; }
+            public List<string> dataSizes { get; set; }
+            public bool exists { get; set; }
+            public int total { get; set; }
+
+            public DataSet (string name)
+            {
+                this.name = name;
+                this.total = 0;
+                dataSizes = new List<string>();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
